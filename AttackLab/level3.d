f173a21c65f851bd7d14db9523e808888160e507
Lavender <z5270643@ad.unsw.edu.au>
2020-08-02 12:59:55 +1000

    update

---
+++ AttackLab/level3.d
@@ -0,0 +1,11 @@
+
+level3.o:     file format elf64-x86-64
+
+
+Disassembly of section .text:
+
+0000000000000000 <.text>:
+   0:	48 8d 7c 24 08       	lea    0x8(%rsp),%rdi
+   5:	48 c7 04 24 fa 18 40 	movq   $0x4018fa,(%rsp)
+   c:	00 
+   d:	c3                   	retq   
